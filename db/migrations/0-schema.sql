555b3c6731f06559f31aaa79834f3c69d6f1d7c0
Dylan <dylanfridman@gmail.com>
2025-02-12 14:46:32 -0500

    Add column to current schema

--- db/migrations/0-schema.sql
+++ db/migrations/0-schema.sql
@@ -100,7 +100,7 @@ CREATE TABLE trip_members (
   leader INTEGER DEFAULT FALSE,
   attended INTEGER DEFAULT FALSE,
   pending INTEGER DEFAULT TRUE,
-  signed_up_on INTEGER DEFAULT (strftime('%s', 'now')),
+  signed_up_on INTEGER DEFAULT NULL,
   PRIMARY KEY (trip, user)
 ) STRICT;
 CREATE TABLE trip_required_gear (
